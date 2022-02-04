7ff76e8512ef8baf66e14adb4a0d3bfc560524f9
baxiche <baxiche@gmail.com>
2022-02-04 16:55:00 +0800

    update 21

--- LinkedList/21.Merge_Two_Sorted_Lists.c
+++ LinkedList/21.Merge_Two_Sorted_Lists.c
@@ -6,7 +6,21 @@
  * };
  */
 
-
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
-    
+    struct ListNode *head;
+    struct ListNode **cur = &head;
+    for (; l1 && l2; cur = &((*cur)->next)){
+        if (l1->val < l2->val){
+            *cur = l1;
+            l1 = l1->next;
+        }
+        else {
+            *cur = l2;
+            l2 = l2->next;
+        }
+    }
+    if (l1) *cur = l1;
+    if (l2) *cur = l2;
+
+    return head;
 }
